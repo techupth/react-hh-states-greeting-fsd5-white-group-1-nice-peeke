7f4f5ae186c383e4a7ba17f26975185ec519c081
Gravizz <peeke0rr@gmail.com>
2023-12-24 13:38:41 +0700

    feat:add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
+import { GreetingMessage } from "./component/GreetingMessage";
 
 function App() {
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
-      <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
-      </div>
+      <GreetingMessage></GreetingMessage>
     </div>
   );
 }
